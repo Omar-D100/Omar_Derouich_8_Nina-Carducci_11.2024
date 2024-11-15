3a770a48e2deae552ddc427d79e78a5c8aa7c795
OmarZoldik <omar10.drh@outlook.fr>
2024-11-15 16:05:20 +0100

    convert jquery to js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".gallery").mauGallery({columns:{xs:1,sm:2,md:3,lg:3,xl:3},lightBox:!0,lightboxId:"myAwesomeLightbox",showTags:!0,tagsPosition:"top"})});
+import o from"./maugallery";document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll(".gallery").forEach(function(t){new o(t,{columns:{xs:1,sm:2,md:3,lg:3,xl:3},lightBox:!0,lightboxId:"myAwesomeLightbox",showTags:!0,tagsPosition:"top"})})});
